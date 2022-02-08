a04e0e926e1b3bf6aaf675fd2576a564411bd5e9
DebdutBiswas <rupam.biswas@outlook.com>
2022-02-08 20:40:40 +0530

    Removed Testing Data

--- src/App.js
+++ src/App.js
@@ -12,46 +12,6 @@ import "./styles.css";
 import "./App.css";
 import "./gutter.css";
 
-// let INITIAL_DATA = [
-//   {
-//     id: 'd63eb334-5f88-4134-b707-3f82116cfc09',
-//     name: 'Name1',
-//     phone: '9091917512'
-//   },
-//   {
-//     id: 'ac74197a-8c5b-48ff-b65d-ae25c0bddc69',
-//     name: 'Name2',
-//     phone: '9412566669'
-//   },
-//   {
-//     id: '3bd99d6d-0017-4b36-9ebd-b55c448e413b',
-//     name: 'Name3',
-//     phone: '9494506346'
-//   },
-//   {
-//     id: 'da66b2c5-dac5-4104-93ad-98ea9bf4216f',
-//     name: 'Name4',
-//     phone: '9582481988'
-//   },
-//   {
-//     id: '5c851670-0710-425a-a0b5-7b02a2f1bf1d',
-//     name: 'Name5',
-//     phone: '9838857116'
-//   },
-//   {
-//     id: '624c883b-406c-4a5e-ad02-68c7981dd274',
-//     name: 'Name6',
-//     phone: '9692916148'
-//   },
-//   {
-//     id: 'f8133262-9f8c-4569-ac28-abe23bc9044a',
-//     name: 'Name7',
-//     phone: '9451332576'
-//   },
-// ];
-
-// INITIAL_DATA = [];
-
 const App = () => {
   const [data, setData] = useState([]);
   const [opCounter, setOpCounter] = useState({ addCount: 0, updateCount: 0, deleteCount: 0 });
